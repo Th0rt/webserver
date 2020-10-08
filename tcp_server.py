4f639ba3c8dfb6f5cadb51e2637b03f6ed5368f3
Tsukasa Yamada <yamada.tsukasa@panair.jp>
2020-10-08 15:57:28 +0900

    スレッドでのhttpレスポンスに対応

--- tcp_server.py
+++ tcp_server.py
@@ -1,7 +1,10 @@
 import os
 import socket
 from enum import Enum
+from threading import Thread
+
 from request import HttpRequest
+from response import HttpResponse
 
 DOCUMENT_ROOT = "./resource/server"
 
@@ -19,17 +22,32 @@ class TcpServer:
             server_socket.listen(1)
             print(ServerMessage.WAITING_CONNECTION.value)
 
-            client_socket, address = server_socket.accept()
-            print(ServerMessage.CONNECTED.value)
+            while True:
+                client_socket, address = server_socket.accept()
+                print(ServerMessage.CONNECTED.value)
 
-            request = HttpRequest(client_socket.recv(4096))
+                request = HttpRequest(client_socket.recv(4096))
 
-            with open("./resource/server/recv.txt", "wb") as f:
-                f.write(request.as_bytes())
+                with open("./resource/server/recv.txt", "wb") as f:
+                    f.write(request.as_bytes())
 
-            with open(DOCUMENT_ROOT + request.header.path, "rb") as f:
-                response = HttpResponse(body=f.read())
+                with open(DOCUMENT_ROOT + request.path, "rb") as f:
+                    content = f.read()
 
+                thread = HttpResponseThread(client_socket, HttpResponse(content, content_type))
+                thread.start()
+
+
+class HttpResponseThread(Thread):
+    def __init__(self, sock, response: HttpResponse, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.socket = sock
+        self.response = response
+
+    def run(self):
+        self.socket.send(self.response.as_bytes())
+        self.socket.close()
+        print(ServerMessage.CONNECTION_CLOSED.value)
 
 
 if __name__ == "__main__":
